81b3cea897e240cca0aaf4bff4f52436a783ecd8
AnimusVonith <jakub.adamciak@gmail.com>
2022-05-04 11:58:41 +0200

    git clean up

--- coderone/update_dev_gym.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-del bomberland_2\python3\dev_gym.py
-copy bomberland\python3\dev_gym.py bomberland_2\python3\dev_gym.py
-del bomberland_3\python3\dev_gym.py
-copy bomberland\python3\dev_gym.py bomberland_3\python3\dev_gym.py
-del bomberland_4\python3\dev_gym.py
-copy bomberland\python3\dev_gym.py bomberland_4\python3\dev_gym.py
-echo update done
--- coderone/update_fwd_model.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-del bomberland_2\python3\forward_model.py
-copy bomberland\python3\forward_model.py bomberland_2\python3\forward_model.py
-del bomberland_3\python3\forward_model.py
-copy bomberland\python3\forward_model.py bomberland_3\python3\forward_model.py
-del bomberland_4\python3\forward_model.py
-copy bomberland\python3\forward_model.py bomberland_4\python3\forward_model.py
-echo update done
